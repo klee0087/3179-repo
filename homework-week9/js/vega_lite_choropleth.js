4d772f4a0b97f99ad9a211df572217b05205a50a
Kevin Lee <90955651+klee0087@users.noreply.github.com>
2021-09-25 15:20:40 +0800

    changed html and js

--- homework-week9/js/vega_lite_choropleth.js
+++ homework-week9/js/vega_lite_choropleth.js
@@ -1,4 +1,4 @@
-var vg_1 = "choropleth_map.json";
+var vg_1 = "https://raw.githubusercontent.com/klee0087/3179-repo/main/homework-week9/js/choropleth_map.json";
 vegaEmbed("#choropleth_map", vg_1).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
